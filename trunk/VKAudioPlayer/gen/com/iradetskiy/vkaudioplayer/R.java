f0de5a9e1cc7731f6d1d2367602987216b5ca3da
change-me <change-me@bfc973e2-121c-724b-b218-94e4328cf4ea>
2012-09-10 14:48:17 +0000

    Basic UI: search strip, empty button onClick function
    git-svn-id: file:///home/debi/svn_repos@3 bfc973e2-121c-724b-b218-94e4328cf4ea

--- trunk/VKAudioPlayer/gen/com/iradetskiy/vkaudioplayer/R.java
+++ trunk/VKAudioPlayer/gen/com/iradetskiy/vkaudioplayer/R.java
@@ -15,19 +15,33 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int artist=0x7f070001;
+        public static final int duration=0x7f070002;
+        public static final int menu_settings=0x7f070007;
+        public static final int searchButton=0x7f070006;
+        public static final int searchLabel=0x7f070004;
+        public static final int searchResultsList=0x7f070003;
+        public static final int searchText=0x7f070005;
+        public static final int song=0x7f070000;
     }
     public static final class layout {
-        public static final int activity_search=0x7f030000;
+        public static final int audio_item=0x7f030000;
+        public static final int search=0x7f030001;
+        public static final int search_strip=0x7f030002;
     }
     public static final class menu {
         public static final int activity_search=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_search=0x7f040003;
+        public static final int menu_download=0x7f040006;
+        public static final int menu_settings=0x7f040001;
+        public static final int menu_to_downloads=0x7f040007;
+        public static final int menu_to_playlist=0x7f040008;
+        public static final int search_button=0x7f040005;
+        public static final int search_hint=0x7f040004;
+        public static final int search_label=0x7f040003;
+        public static final int title_activity_search=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
--- trunk/VKAudioPlayer/src/com/iradetskiy/vkaudioplayer/SearchActivity.java
+++ trunk/VKAudioPlayer/src/com/iradetskiy/vkaudioplayer/SearchActivity.java
@@ -3,18 +3,23 @@ package com.iradetskiy.vkaudioplayer;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
 
 public class SearchActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.search);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_search, menu);
         return true;
+    }
+    
+    public void onSearchButtonClick(View v){
+    	
     }
 }
