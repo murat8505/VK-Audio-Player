f77cc4279602443ffe00b7913b03d46cbd53081a
debi <debi@bfc973e2-121c-724b-b218-94e4328cf4ea>
2012-12-11 21:37:03 +0000

    Youtrack Issue 1-9 fixed.
    git-svn-id: file:///home/debi/svn_repos@28 bfc973e2-121c-724b-b218-94e4328cf4ea

---
+++ trunk/VKAudioPlayer/src/com/iradetskiy/vkaudioplayer/MusicControlActivity.java
@@ -0,0 +1,99 @@
+package com.iradetskiy.vkaudioplayer;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+
+public class MusicControlActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+
+    private boolean isPlaying = true;
+    private SeekBar seekBar;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.music_control);
+
+        seekBar = (SeekBar)findViewById(R.id.seek_bar);
+        seekBar.setOnSeekBarChangeListener(this);
+        seekBar.setMax(1000);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(PlayMusicService.ACTION_SEEK_TO);
+        filter.addAction(PlayMusicService.ACTION_BUFFERING_UPDATE);
+        registerReceiver(seekReceiver, filter);
+    }
+
+    public void onPlayPressed(View v) {
+        if (isPlaying) {
+            ((ImageButton)v).setImageResource(android.R.drawable.ic_media_pause);
+            startService(new Intent(PlayMusicService.ACTION_PAUSE));
+        }
+        else {
+            ((ImageButton)v).setImageResource(android.R.drawable.ic_media_play);
+            startService(new Intent(PlayMusicService.ACTION_PLAY));
+        }
+        isPlaying = !isPlaying;
+    }
+
+    public void onPrevPressed(View v) {
+
+    }
+
+    public void onNextPressed(View v) {
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(seekReceiver);
+        startService(new Intent(PlayMusicService.ACTION_KILL));
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+        if (b) {
+            Intent intent = new Intent();
+            intent.setAction(PlayMusicService.ACTION_SEEK_TO);
+            intent.putExtra("seek_to", i);
+
+            startService(intent);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private BroadcastReceiver seekReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(PlayMusicService.ACTION_SEEK_TO)) {
+                int seekTo = intent.getIntExtra("seek_to", -1);
+                if (seekTo != -1) {
+                    seekBar.setProgress(seekTo);
+                }
+            }
+            else if (action.equals(PlayMusicService.ACTION_BUFFERING_UPDATE)) {
+                int secondaryProgress = intent.getIntExtra("buffering_update", -1);
+                if (secondaryProgress != -1) {
+                    seekBar.setSecondaryProgress(secondaryProgress * 10);
+                }
+            }
+        }
+    };
+}
---
+++ trunk/VKAudioPlayer/src/com/iradetskiy/vkaudioplayer/PlayMusicService.java
@@ -0,0 +1,125 @@
+package com.iradetskiy.vkaudioplayer;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.AssetFileDescriptor;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.IOException;
+
+
+public class PlayMusicService extends Service implements MediaPlayer.OnPreparedListener, MediaPlayer.OnBufferingUpdateListener{
+
+    public static final String ACTION_PLAY = "com.example.PlayMusicService.ACTION_PLAY";
+    public static final String ACTION_PAUSE = "com.example.PlayMusicService.ACTION_PAUSE";
+    public static final String ACTION_NEXT = "com.example.PlayMusicService.ACTION_NEXT";
+    public static final String ACTION_PREVIOUS = "com.example.PlayMusicService.ACTION_PREVIOUS";
+    public static final String ACTION_KILL = "com.example.PlayMusicService.ACTION_KILL";
+    public static final String ACTION_SEEK_TO = "com.example.PlayMusicService.ACTION_SEEK_TO";
+    public static final String ACTION_BUFFERING_UPDATE = "com.example.PlayMusicService.ACTION_BUFFERING_UPDATE";
+
+    private boolean needToNotify = false;
+    private MediaPlayer mediaPlayer;
+
+    private Thread musicControlNotifier = new Thread() {
+        @Override
+        public void run() {
+            while(needToNotify) {
+                try {
+                    if (mediaPlayer.isPlaying()) {
+                        Intent intent = new Intent();
+                        intent.setAction(ACTION_SEEK_TO);
+                        intent.putExtra("seek_to", (mediaPlayer.getCurrentPosition() * 1000) / mediaPlayer.getDuration());
+                        sendBroadcast(intent);
+                    }
+                    sleep(1000);
+                } catch (InterruptedException e) {
+                    Log.d("PlayMusicService", "sleep error");
+                }
+            }
+        }
+    };
+
+    @Override
+    public void onCreate() {
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setOnPreparedListener(this);
+        mediaPlayer.setOnBufferingUpdateListener(this);
+        needToNotify = true;
+        musicControlNotifier.start();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        String action = intent.getAction();
+
+        if (action.equals(ACTION_PLAY)) {
+
+            String uri = intent.getStringExtra(CurrentUserAudioActivity.from[3]);
+
+            if (uri != null) {
+
+                //AssetFileDescriptor fileDescriptor = getResources().openRawResourceFd(trackId);
+                try {
+                    mediaPlayer.setDataSource(this, Uri.parse(uri));
+                    //fileDescriptor.close();
+                    mediaPlayer.prepareAsync();
+                } catch (IOException e) {
+                    Log.d("PlayMusicService", "Trouble with playing");
+                }
+            }
+            else {
+                mediaPlayer.start();
+            }
+        }
+        else if (action.equals(ACTION_PAUSE)) {
+            mediaPlayer.pause();
+        }
+        else if (action.equals(ACTION_KILL)) {
+            stopSelf();
+        }
+        else if (action.equals(ACTION_SEEK_TO)) {
+            int seekTo = intent.getIntExtra("seek_to", -1);
+            if (seekTo != -1) {
+                int duration = mediaPlayer.getDuration();
+
+                mediaPlayer.seekTo((seekTo * duration) / 1000);
+            }
+        }
+
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        mediaPlayer.release();
+        needToNotify = false;
+        Toast.makeText(this, "PlayMusicService killed", Toast.LENGTH_SHORT).show();
+    }
+
+
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onPrepared(MediaPlayer mediaPlayer) {
+        Log.d("PlayMusicService", "Audio prepared...");
+        mediaPlayer.start();
+    }
+
+    @Override
+    public void onBufferingUpdate(MediaPlayer mediaPlayer, int i) {
+        Intent intent = new Intent();
+        intent.setAction(ACTION_BUFFERING_UPDATE);
+        intent.putExtra("buffering_update", i);
+        sendBroadcast(intent);
+    }
+}
